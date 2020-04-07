79df05e961eab95e8cf436d87865dc1e4d48bc4e
LukeBogdanovic <58079356+LukeBogdanovic@users.noreply.github.com>
2020-04-07 18:28:46 +0100

    version2.0.4

--- CS4222_Programming_Assignment/src/wordsearch/Driver2.java
+++ CS4222_Programming_Assignment/src/wordsearch/Driver2.java
@@ -2,20 +2,32 @@ package wordsearch;
 import java.util.*;
 
 public class Driver2 {
+	public static char[][] puzzle;
 
 	public static void main(String[] args) {
 		List<String> words = new ArrayList<String>();
-		words.add("cause");
-		words.add("under");
-		words.add("station");
-		words.add("whenever");
-		words.add("nope");
-		words.add("checked");
-		words.add("whatever");
+		words.add("cause".toUpperCase());
+		words.add("under".toUpperCase());
+		words.add("station".toUpperCase());
+		words.add("whenever".toUpperCase());
+		words.add("nope".toUpperCase());
+		words.add("checked".toUpperCase());
+		words.add("whatever".toUpperCase());
 		WordSearchPuzzle call = new WordSearchPuzzle(words);
+		puzzle = call.getPuzzleAsGrid();
+		getGrid(puzzle);
 		call.getPuzzleAsGrid();
 		call.showWordSearchPuzzle(false);
 
+	}
+	
+	private static void getGrid(char[][] Grid) {
+		for(int i = 0; i < puzzle.length; i++) {
+			for(int j = 0; j < puzzle[0].length;j++) {
+				System.out.print(puzzle[i][j] + " ");
+		    }
+		System.out.println();
+		}
 	}
 
 }
